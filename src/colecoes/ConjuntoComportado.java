77cc3ff0707201ce3993cf60f6019adbd0dbd8c2
Imer Jr <softwareenginner.imerjr@gmail.com>
2021-09-25 18:16:01 -0300

    Correção do Projeto Java Exemplos no Git

---
+++ src/colecoes/ConjuntoComportado.java
@@ -0,0 +1,41 @@
+package colecoes;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class ConjuntoComportado {
+
+	public static void main(String[] args) {
+
+		// Set<String> listaAprovados = new HashSet<String>(); //pode ser vazio o parâmetro da Generics do lado direito,
+												   //como: Set<String> lista = new HashSet<>();
+		SortedSet<String> listaAprovados = new TreeSet<String>();
+		
+		listaAprovados.add("Ana");
+		listaAprovados.add("Carlos");
+		listaAprovados.add("Luca");
+		listaAprovados.add("Pedro");
+		
+		for(String candidato: listaAprovados) {
+			System.out.println(candidato);
+		}
+		
+
+		Set<Integer> nums = new HashSet<>(); //pode ser vazio o parâmetro da Generics do lado direito,
+												   //como: Set<String> lista = new HashSet<>();
+		
+		nums.add(1);
+		nums.add(2);
+		nums.add(3);
+		nums.add(4);
+		
+		for(int num: nums) {
+			System.out.println(num);
+		}
+		
+		
+	}
+
+}
---
+++ src/colecoes/ConjuntoMisturado.java
@@ -0,0 +1,52 @@
+package colecoes;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ConjuntoMisturado {
+	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public static void main(String[] args) {
+		
+		HashSet conjunto = new HashSet();
+		
+		conjunto.add(1.2); //double -> Double
+		conjunto.add(true); //boolean -> Boolean
+		conjunto.add("Teste"); //String
+		conjunto.add(1); //int -> Integer
+		conjunto.add('x'); //char -> Character
+		
+		System.out.println("Tamanho é " + conjunto.size());
+		
+		conjunto.add("teste");
+		conjunto.add('x');
+		System.out.println("Tamanho é " + conjunto.size());
+		
+		System.out.println(conjunto.remove("teste"));
+		System.out.println(conjunto.remove("Teste"));
+		System.out.println(conjunto.remove('x'));
+		
+		System.out.println("Tamanho é " + conjunto.size());
+		
+		System.out.println(conjunto.contains('x'));
+		System.out.println(conjunto.contains(1));
+		System.out.println(conjunto.contains(true));
+		
+		Set nums = new HashSet();
+		
+		nums.add(1);
+		nums.add(2);
+		nums.add(3);
+		
+		System.out.println(nums);
+		System.out.println(conjunto);
+		
+		//conjunto.addAll(nums);	// União entre dois conjuntos
+		conjunto.retainAll(nums);
+		System.out.println(conjunto);
+		
+		conjunto.clear();
+		System.out.println(conjunto);
+	}
+
+}
